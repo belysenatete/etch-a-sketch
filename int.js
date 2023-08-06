9ec87f787a2e0155dd2733bce012554846d10eea
Nate Belyz <natetebelyse12@gmail.com>
2023-08-06 16:24:41 +0200

    fixed the mousedown event on the board

--- int.js
+++ int.js
@@ -10,21 +10,31 @@ function createBoard(size){
   for(let i=0; i<numDivs; i++ ){
    const div = document.createElement('div')
    div.classList.add('board-cells');
-   color.addEventListener('change', function(){
-    div.addEventListener('mouseover', function(){
-        const selectedColor = color.value;
-        div.style.backgroundColor = selectedColor;
-    })
-   })
+      
+  // Mouse down event to start drawing
+  div.addEventListener("mousedown", function() {
+    draw = true; // Allow drawing
+    const selectedColor = color.value;
+    div.style.backgroundColor = selectedColor; // Color the cell when you first click it
+});
+ // Mouse over event to draw when moving
+ div.addEventListener("mouseover", function() {
+  if(draw) { // Check if drawing is allowed
+      const selectedColor = color.value;
+      div.style.backgroundColor = selectedColor;
+  }
+}); 
+div.addEventListener("mouseup", function() {
+  draw = false;
+});    
+
    board.insertAdjacentElement("beforeend", div);
 
   }
-  const clear = document.getElementById('clearBtn');
-  clear.addEventListener('click', function(){
   
-  })
 }
 createBoard(16);
+
 //window prompt
 function newResolution(){
   const input = prompt("please select a resolution from 1 to 100");
@@ -44,17 +54,19 @@ resolution.addEventListener("click", function(){
   const size = newResolution();
   createBoard(size);
 })
-//reseting
+
+//reseting the board
 
 function clearBoard(){
   const divs = document.querySelectorAll('.board-cells');
   divs.forEach((div) => div.style.backgroundColor = 'white')
 }
+
+// deleting the unwanted
+
 function erase(){
-  draw = false;
   color.value = '#ffffff'
 }
-eraserBtn.addEventListener('click', erase);
 
 
 
